ee100fa7b06b668fc3ad63cf4833e89a6a393493
Pengfei Meng <Pengfei@Pengfeis-MacBook-Air.local>
2016-09-21 22:41:04 -0400

    matrix right

--- main_interior.m
+++ main_interior.m
@@ -8,20 +8,25 @@ function main_interior()
 % L(x) = f(x) - (1-mu) ln(S) + lam_h h(x) + lam_g (g(x)-s)
 % mu : 1 --> 0
 
-mu = 0; 
+mu = 1; 
 epsilon_mu = 1e-2;     epsilon_tol = 1e-6; 
 theta = 0.5; 
 
-x = [1;1;1;1];     lam = ones(6,1);
+n = 4;    m = 3; 
+x = [1;1;1;1];     lam = ones(3,1);
+s = [4; 6; 1]; 
 
-s = ones(3,1); 
-[f,g] = objfun(x); 
-[cie,Gcie]= coninequ(x); 
-
-
-
-
-
+for k = 1:80
+  dx = kkt_matrix(x, s, lam, mu); 
+  x = x + dx(1:n);
+  s = s + dx(n+1:n+m);
+  lam = lam + dx(n+m+1:end); 
+  if any(s<0)
+      s
+      pause
+  end
+end
+x
 
 % options = optimoptions(@fmincon,'Algorithm','interior-point',...
 %     'GradObj','on',...
@@ -31,25 +36,29 @@ s = ones(3,1);
 end
 
 
-function [kkt_mat, kkt_rhs] = kkt_matrix(x, lam, S)
+function dx = kkt_matrix(x, S, lam, mu)
 
     n = length(x);
     m = length(S);
     
     [fobj,gobj,hobj] = objfun(x); 
-    [cineq,Gcineq]= coninequ(x); 
-
-    kkt_mat = [hobj,           zeros(n, m),  Gcineq; 
-                  zeros(m,n),  diag(lam./S), eye(m); 
-                  Gcineq',      eye(m),      zeros(m)]; 
+    [cineq, ~, Gcineq, ~, hcineq]= coninequ(x);
+    hlag = hobj+lam(1).*hcineq{1} + lam(2).*hcineq{2} + lam(3).*hcineq{3}; 
+    
+    kkt_mat = [hlag,        zeros(n, m),  Gcineq; 
+               zeros(m,n),  diag(lam./S), eye(m); 
+               Gcineq',      eye(m),      zeros(m)]; 
             
-    kkt_rhs = -[gobj + lam(1:m/2) - lam(m/2+1:end);
-                -mu.*(1./S) - lam; 
-                cineq - S];
+    kkt_rhs = -[gobj + Gcineq*lam;
+                -mu.*(1./S) + lam; 
+                cineq + S];
             
-    dx = gmres(kkt_matrix, kkt_rhs);       
+    dx = gmres(kkt_mat, kkt_rhs);       
 
-
+    c_s = cineq + S
+    norm(kkt_rhs, Inf)
+    norm(kkt_mat)
+    
 end
 
 function [f,g,h] = objfun(x)
@@ -95,9 +104,13 @@ Hcineq = cell(1, length(cineq));
 Hcineq{1} = diag([-2, -2, -2, -2]);
 Hcineq{2} = diag([-2, -4, -2, -4]);
 Hcineq{3} = diag([-4, -2, -2,  0]);
+
 % % if fmincon, or the interior points on paper:   c<0
-% cineq = -cineq;
-% Gcineq = -Gcineq; 
+cineq = -cineq;
+Gcineq = -Gcineq; 
+Hcineq{1} = -1*Hcineq{1}; 
+Hcineq{2} = -1*Hcineq{2};
+Hcineq{3} = -1*Hcineq{3};
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
